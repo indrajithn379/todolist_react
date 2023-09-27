9335c6063098db1d33386deb7bac938d23754170
indrajithn379 <indrajithn379@gmail.com>
2023-09-27 10:56:55 +0530

    Updates

---
+++ precache-manifest.0543d7adf1b9d6e4c0ec601c5e6471fd.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "5204526f7c2468486594",
+    "url": "/todolist_react/static/css/main.e2b7043f.chunk.css"
+  },
+  {
+    "revision": "5204526f7c2468486594",
+    "url": "/todolist_react/static/js/main.8a98277f.chunk.js"
+  },
+  {
+    "revision": "fa5472fe5f4730ec7588",
+    "url": "/todolist_react/static/js/runtime~main.27a3d799.js"
+  },
+  {
+    "revision": "4d0fcc3bfee1dac1c513",
+    "url": "/todolist_react/static/js/2.06e4ed5a.chunk.js"
+  },
+  {
+    "revision": "11a8b0d94f2b62360a7b18e0e151d69b",
+    "url": "/todolist_react/index.html"
+  }
+];
